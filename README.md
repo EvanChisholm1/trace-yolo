b37992e41436974b4f6f3fa58dee23421ba4e105
Evan Chisholm <elchisholm248@gmail.com>
2025-03-02 11:17:21 -0500

    did some graphing :)

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Trace with YOLO
 
 An experimental new version of Trace my ski racing video analysis tool, using Yolo for object detection.
+
+ideas
+
+-   in a lot cases width of the bounding box should be roughly periodic function
+-   once the bounding box reaches a local min (the transition of the turn) switch the edge
--- track_video.py
+++ track_video.py
@@ -3,10 +3,15 @@ import supervision as sv
 from inference.core.utils.image_utils import load_image_bgr
 import cv2
 import os
+import json
 
 images = os.listdir('./imgs')
 
 i = 0
+
+most_recent_p = None
+
+posns = []
 
 model = get_model(model_id="yolov8n-640")
 for img in images:
@@ -16,8 +21,16 @@ for img in images:
     image = load_image_bgr(image)
 
     results = model.infer(image)[0]
+    # print(results.predictions)
+    results.predictions = [p for p in results.predictions if p.class_name == "person" ]
+    if len(results.predictions) > 0 and results.predictions[0]:
+        pred = results.predictions[0]
+        most_recent_p = pred
+        print(pred.x)
+        posns.append({'x': pred.x, 'width': pred.width})
     results = sv.Detections.from_inference(results)
-    print(results)
+    # print(results[0])
+    # results = results.filter(results.class_id == 0)
     annotator = sv.BoxAnnotator(thickness=4)
     annotated_image = annotator.annotate(image, results)
     annotator = sv.LabelAnnotator(text_scale=2, text_thickness=2)
@@ -25,5 +38,7 @@ for img in images:
     # sv.plot_image(annotated_image)
     cv2.imwrite(f"./out/{img}", annotated_image)
 
+
+json.dump(posns, open('posns.json', 'w'))
 
 # image = './test1.png'
